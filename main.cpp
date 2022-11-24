093f2e40a8cd02b0c8c925a8b33f084ae73ce8f0
farinaema <117036663+farinaema@users.noreply.github.com>
2022-11-24 22:38:06 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-   return 0;
-}
+int main(){
+
+    int n, i, a, b, c;
+    cin >> n;
+    a=1;
+    b=1;
+    cout << a << endl << b << endl;
+        for (i=0; i<=n; i=i+a){
+            c=a+b;
+            a=b;
+            b=c;
+            cout << c << endl;
+        }
+    }
